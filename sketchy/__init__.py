fbf2431ba34b595faf8e7ee214ee839de707cb8a
achan1989 <achan1989@gmail.com>
2018-01-07 11:57:48 +0000

    Fix controls.
    * Does not assume a controller is connected.
    * Does sensible things if both a mouse and controller are connected, and you
      switch between them or press buttons on both.

--- sketchy/__init__.py
+++ sketchy/__init__.py
@@ -13,11 +13,13 @@ BACKGROUND_ALPHA = 45
 CURRENT_RESOLUTION = (0, 0)
 TARGET_FRAMERATE = 30
 
-STICK_DEADZONE = 0.2
-STICK_SPEED_MULT = 2
-X_AXIS_IDX = 0
-Y_AXIS_IDX = 1
-SHAKE_BUTTON_IDX = 3
+JOY_DEADZONE = 0.2
+JOY_SPEED_MULT = 2
+JOY_X_AXIS_IDX = 0
+JOY_Y_AXIS_IDX = 1
+JOY_SHAKE_BUTTON_IDX = 3
+
+MOUSE_SPEED_MULT = 0.5
 
 # RGBA
 LINE_COLOUR = (65, 65, 65, 255)
@@ -35,12 +37,9 @@ def main():
     bg_surface = init_background(screen)
     pygame.display.flip()
 
-    # x,y position of the cursor.
-    pos = (0,0)
-
-    joysticks = init_joysticks()
-
+    controls = Controls(bounds)
     quit = False
+
     while not quit:
         for evt in pygame.event.get():
             if evt.type == QUIT:
@@ -48,40 +47,19 @@ def main():
             if evt.type == KEYDOWN and evt.key == K_ESCAPE:
                 quit = True
 
-        for stick in joysticks:
-            stick.update()
-
-        new_pos = do_move(pos, joysticks[0], screen, bounds)
-        pos = new_pos
-
-        if joysticks[0].shaking == ButtonState.ON:
-            do_shake(screen, bg_surface)
+        controls.update()
+        if controls.changed:
+            draw_move(controls, screen)
+            if controls.shaking == ButtonState.ON:
+                do_shake(screen, bg_surface)
 
         clock.tick(TARGET_FRAMERATE)
 
-def do_move(old_pos, stick, screen, bounds):
-    moving = math.sqrt(stick.curr_x**2 + stick.curr_y**2) >= STICK_DEADZONE
-    if not moving:
-        return old_pos
-
-    old_x, old_y = old_pos
-    new_x = old_x + (stick.curr_x * STICK_SPEED_MULT)
-    new_y = old_y + (stick.curr_y * STICK_SPEED_MULT)
-    bounds_x, bounds_y = bounds
-    if new_x < 0:
-        new_x = 0
-    elif new_x > bounds_x:
-        new_x = bounds_x
-    if new_y < 0:
-        new_y = 0
-    elif new_y > bounds_y:
-        new_y = bounds_y
-    new_pos = (new_x, new_y)
-
-    dirty = pygame.draw.line(screen, LINE_COLOUR, old_pos, new_pos, LINE_WIDTH)
+def draw_move(controls, screen):
+    dirty = pygame.draw.line(
+        screen, LINE_COLOUR, controls.old_pos, controls.pos, LINE_WIDTH)
     dirty.inflate_ip(LINE_WIDTH*2, LINE_WIDTH*2)
     pygame.display.update(dirty)
-    return new_pos
 
 def do_shake(screen, bg_surface):
     screen.blit(bg_surface, (0,0))
@@ -115,46 +93,110 @@ def init_background(screen):
     bg_surface.get_view().write(bg_data)
     return bg_surface
 
-def init_joysticks():
-    joysticks = [Joystick(x)
-                 for x in range(pygame.joystick.get_count())]
-    for stick in joysticks:
-        stick.debug_print()
-    return joysticks
+def clamp(n, low, high):
+    if n < low:
+        n = low
+    elif n > high:
+        n = high
+    return n
 
 
-class Joystick:
-    def __init__(self, jid):
-        self._stick = pygame.joystick.Joystick(jid)
-        self._stick.init()
-        self.curr_x = 0.0
-        self.curr_y = 0.0
+class Controls:
+    def __init__(self, bounds):
+        pygame.mouse.set_visible(False)
+        pygame.event.set_grab(True)
+        # Reset relative counters.
+        pygame.mouse.get_rel()
+
+        self.joysticks = [
+            pygame.joystick.Joystick(jid)
+            for jid in range(pygame.joystick.get_count())]
+        for stick in self.joysticks:
+            stick.init()
+
+        self.bounds_x, self.bounds_y = bounds
+        self.old_pos = (self.bounds_x/2, self.bounds_y/2)
+        self.pos = self.old_pos
+        self._m_shaking = ButtonState.OFF
+        self._j_shaking = ButtonState.OFF
         self.shaking = ButtonState.OFF
-        self.update()
-
-    def __getattr__(self, name):
-        return getattr(self._stick, name)
+        self.changed = False
 
     def update(self):
-        self.curr_x = self.get_axis(X_AXIS_IDX)
-        self.curr_y = self.get_axis(Y_AXIS_IDX)
+        got_input = False
+        mouse_input = False
+        joy_input = False
 
-        shake_on = self.get_button(SHAKE_BUTTON_IDX)
-        if shake_on:
-            if self.shaking == ButtonState.OFF:
-                self.shaking = ButtonState.ON
-            elif self.shaking == ButtonState.ON:
-                self.shaking = ButtonState.HELD
+        m_dx, m_dy = pygame.mouse.get_rel()
+        _l, _m, m_shake_input = pygame.mouse.get_pressed()
+        m_shaking_change = self.shaking_change(m_shake_input, self._m_shaking)
+        if m_shaking_change is not None:
+            self._m_shaking = m_shaking_change
+        if (m_dx, m_dy) != (0, 0) or m_shake_input or m_shaking_change is not None:
+            mouse_input = True
+
+        for stick in self.joysticks:
+            j_dx = stick.get_axis(JOY_X_AXIS_IDX)
+            j_dy = stick.get_axis(JOY_Y_AXIS_IDX)
+            j_shake_input = stick.get_button(JOY_SHAKE_BUTTON_IDX)
+            j_shaking_change = self.shaking_change(j_shake_input, self._j_shaking)
+            if j_shaking_change is not None:
+                self._j_shaking = j_shaking_change
+            moving = math.sqrt(j_dx**2 + j_dy**2) >= JOY_DEADZONE
+            if not moving:
+                j_dx = 0
+                j_dy = 0
+            if moving or j_shake_input or j_shaking_change is not None:
+                joy_input = True
+                break
+
+        if mouse_input:
+            dx = m_dx
+            dy = m_dy
+            shaking = self._m_shaking
+            move_mult = MOUSE_SPEED_MULT
+        elif joy_input:
+            dx = j_dx
+            dy = j_dy
+            shaking = self._j_shaking
+            move_mult = JOY_SPEED_MULT
+
+        if mouse_input or joy_input:
+            old_x, old_y = self.pos
+            new_x = clamp(old_x + (dx * move_mult), 0, self.bounds_x)
+            new_y = clamp(old_y + (dy * move_mult), 0, self.bounds_y)
+            self.old_pos = self.pos
+            self.pos = (new_x, new_y)
+
+            self.shaking = shaking
+            self.changed = True
         else:
-            self.shaking = ButtonState.OFF
+            self.changed = False
 
-    def debug_print(self):
-        print("stick {}: {}".format(self.get_id(), self.get_name()))
-        axes = self.get_numaxes()
+    @staticmethod
+    def shaking_change(shake_input, last_state):
+        if shake_input:
+            if last_state == ButtonState.OFF:
+                state_change = ButtonState.ON
+            elif last_state == ButtonState.ON:
+                state_change = ButtonState.HELD
+            else:
+                state_change = None
+        else:
+            if last_state in (ButtonState.ON, ButtonState.HELD):
+                state_change = ButtonState.OFF
+            else:
+                state_change = None
+        return state_change
+
+
+def joy_debug_print(stick):
+        print("stick {}: {}".format(stick.get_id(), stick.get_name()))
+        axes = stick.get_numaxes()
         for i in range(axes):
-            print("Axis {}: {}".format(i, self.get_axis(i)))
-        buttons = [self.get_button(i)
-                   for i in range(self.get_numbuttons())]
+            print("Axis {}: {}".format(i, stick.get_axis(i)))
+        buttons = [stick.get_button(i)
+                   for i in range(stick.get_numbuttons())]
         print("Buttons: ", buttons)
 
 
